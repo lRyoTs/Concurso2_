aa959793f6a70d544ac64c73a8023d9055c87995
danielfajardomaqueira <d.fajardomaqueira@gmail.com>
2023-03-21 12:59:55 +0100

    V03_

--- Assets/music_manager.cs
+++ Assets/music_manager.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class music_manager : MonoBehaviour
 {
 
     public AudioClip[] Songs;
 
-    public int currentSong;
+    private int currentSong;
 
     private AudioSource _audioSource;
+
+    public TextMeshProUGUI songText;
 
     // Start is called before the first frame update
     void Start()
     {
         currentSong = 0;
         _audioSource.clip = Songs[currentSong];
+        UpdateSongName();
     }
 
     // Update is called once per frame
@@ -26,15 +30,13 @@ public class music_manager : MonoBehaviour
 
     private void Awake()
     {
-        _audioSource.GetComponent<AudioSource>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void PlaySong()
     {
         _audioSource.clip = Songs[currentSong];
         _audioSource.Play();
-
-
     }
     
     public void NextSong()
@@ -45,6 +47,7 @@ public class music_manager : MonoBehaviour
             currentSong = 0;
         }
         PlaySong();
+        UpdateSongName();
     }
 
     public void PreviousSong() {
@@ -54,6 +57,18 @@ public class music_manager : MonoBehaviour
             currentSong = Songs.Length - 1;
         }
         PlaySong();
+        UpdateSongName();
+    }
+
+    public void RandomSong()
+    {
+        currentSong = Random.Range(0,Songs.Length);
+        PlaySong();
+        UpdateSongName();
+    }
+
+    public void UpdateSongName() {
+        songText.text = Songs[currentSong].name;
     }
 
 }
